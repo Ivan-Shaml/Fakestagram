d65a1c525392af3a1afafa6c16e9e4fed72b015a
Ivan-Shaml <stu1901681077@uni-plovdiv.bg>
2022-03-13 19:52:28 +0200

    Created 'GenericService' class and interface, exposing the CRUD methods of it's repository.

---
+++ Services/Contracts/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using Fakestagram.Models;
+
+namespace Fakestagram.Services.Contracts
+{
+    public interface IGenericService<TModel, TCreateDTO, TEditDTO, TReadDTO>
+        where TModel : BaseModel
+        where TCreateDTO : class
+        where TEditDTO : class
+        where TReadDTO : class
+    {
+        List<TReadDTO> GetAll();
+        TReadDTO GetById(Guid id);
+        TReadDTO Insert(TCreateDTO createDto);
+        TReadDTO Update(TEditDTO updateDto);
+        void Delete(Guid id);
+    }
+}
---
+++ Services/GenericService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Fakestagram.Data.Repositories.Contracts;
+using Fakestagram.Models;
+using Fakestagram.Services.Contracts;
+
+namespace Fakestagram.Services
+{
+    public class GenericService<TModel, TCreateDTO, TEditDTO, TReadDTO> : IGenericService<TModel, TCreateDTO, TEditDTO, TReadDTO>
+        where TModel : BaseModel
+        where TCreateDTO : class
+        where TEditDTO : class
+        where TReadDTO : class
+    {
+        protected readonly IGenericRepository<TModel> _repo;
+        protected readonly IMapper _mapper;
+
+        public GenericService(IGenericRepository<TModel> repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual List<TReadDTO> GetAll() => _repo.GetAll().Select(entity => _mapper.Map<TModel, TReadDTO>((entity))).ToList();
+
+        public virtual TReadDTO GetById(Guid id)
+        {
+            TModel entity = _repo.GetById(id);
+
+            if (entity is null)
+            {
+                return null;
+            }
+
+            return _mapper.Map<TModel, TReadDTO>(entity);
+        }
+
+        public virtual TReadDTO Insert(TCreateDTO createDto)
+        {
+            TModel entity = _mapper.Map<TCreateDTO, TModel>(createDto);
+            
+            _repo.Create(entity);
+            
+            return _mapper.Map<TModel, TReadDTO>(entity);
+        }
+
+        public virtual TReadDTO Update(TEditDTO updateDto)
+        {
+            TModel entity = _mapper.Map<TEditDTO, TModel>(updateDto);
+
+            _repo.Update(entity);
+
+            return _mapper.Map<TModel, TReadDTO>(entity);
+        }
+    }
+}
